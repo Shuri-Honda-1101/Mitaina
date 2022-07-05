3422c49e097955f80f352c588c097278b0a1d8b7
Shuri-Honda-1101 <knh065146@gmail.com>
2022-07-06 08:15:01 +0900

    フロントの参照path変更

--- mitaina-back/index.ts
+++ mitaina-back/index.ts
@@ -5,7 +5,7 @@ import prisma from './src/lib/prisma';
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, '../mitaina-front/build')));
+app.use(express.static(path.join(__dirname, '../../mitaina-front/build')));
 
 app.get('/users', async (req: Request, res: Response) => {
 	const users = await prisma.user.findMany();
@@ -17,7 +17,7 @@ app.get('/api', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+	res.sendFile(path.join(__dirname, '../../mitaina-front/build/index.html'));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
