fe6eabbee18e084d7dab5bfffc9676ac020bd1dd
Shuri-Honda-1101 <knh065146@gmail.com>
2022-07-06 07:39:48 +0900

    build先変更

--- mitaina-back/index.ts
+++ mitaina-back/index.ts
@@ -1,16 +1,23 @@
-import express, { Request, Response } from "express";
-import prisma from "./src/lib/prisma";
+import express, { Request, Response } from 'express';
+import path from 'path';
+import prisma from './src/lib/prisma';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get("/users", async (req: Request, res: Response) => {
-  const users = await prisma.user.findMany();
-  return res.json(users);
+app.use(express.static(path.join(__dirname, '../mitaina-front/build')));
+
+app.get('/users', async (req: Request, res: Response) => {
+	const users = await prisma.user.findMany();
+	return res.json(users);
 });
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello World!" });
+app.get('/api', (req, res) => {
+	res.json({ message: 'Hello World!' });
+});
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
