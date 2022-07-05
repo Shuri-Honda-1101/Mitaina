19b5fe784b107be202678a4c55421f4f6516cad3
Shuri-Honda-1101 <knh065146@gmail.com>
2022-07-06 06:01:33 +0900

    recoil,eslint,tailwind,prettierのインストール,backとfrontの連携設定

--- mitaina-front/src/App.tsx
+++ mitaina-front/src/App.tsx
@@ -1,45 +1,36 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import "./App.css";
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
+
+interface Users {
+  id?: number;
+  email?: string;
+  name?: string;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+  const [users, setUsers]: [Users[], Dispatch<SetStateAction<[]>>] = useState(
+    []
+  );
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+  useEffect(() => {
+    fetch("/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  console.log(users);
+  console.log(message);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <p>{message}</p>
+      {users.length && <p>{users[0].name}</p>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
